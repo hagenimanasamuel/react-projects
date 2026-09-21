b259d68951c867cef9d4f47fc1c8ca577b379f92
hagenimanasamuel <smlhagenimana@gmail.com>
2026-09-21 19:27:11 +0200

    created the timer functionalities and also added the needed functions to make the timer works as usual

--- learning-react/src/components/Learning-exercises/Timer.jsx
+++ learning-react/src/components/Learning-exercises/Timer.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import "./timer.css";
+
 export default function Timer() {
-    return(
+    const [time, setTime] = useState(0);
+    const [running, setRunning] = useState(false);
+
+    useEffect(() => {
+        if(!running || time <= 0) return
+        const id = setTimeout(() => {
+            setTime(prev => prev - 1);
+        }, 1000);
+        return () => clearTimeout(id);
+    }, [running, time]);
+
+    const start = () => {
+        if(time > 0) setRunning(true);
+    }
+
+    const stop = () => setRunning(false);
+
+    const reset = () => {
+        setRunning(false);
+        setTime(0);
+    }
+    return (
         <>
-            <h1>Timer is running</h1>
+            <h1>Timer</h1>
+            <input type="number" placeholder="seconds to count down" value={time} onChange={(e) => setTime(Number(e.target.value) || 0)} disabled={running}/>
+            <button onClick={start} disabled={running || time <= 0}>Start Timer</button>
+            <button onClick={stop} disabled={!running}>Pause</button>
+            <button onClick={reset}>Reset</button>
+            <h1>{time}</h1>
         </>
     )
 }
