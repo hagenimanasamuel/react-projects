41d7484d6ef11950f0a2ea3d38fdcdb82d2b1b05
hagenimanasamuel <smlhagenimana@gmail.com>
2026-09-22 19:32:23 +0200

    created the to do app which add items using react states

--- learning-react/src/components/Learning-exercises/ToDo.jsx
+++ learning-react/src/components/Learning-exercises/ToDo.jsx
@@ -1,12 +1,30 @@
 import { useEffect, useState } from "react";
+import "./todo.css";
 
 export default function ToDo() {
+    const [input, setInput] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    function addTask() {
+        setTasks([...tasks, input]);
+        setInput('');
+    }
+
+    useEffect(() => {
+
+    })
 
     return (
         <>
             <h1>To-Do App</h1>
-            <input type="text" placeholder="input task" />
-            <button>Add Task</button>
+            <input type="text" placeholder="input task" value={input} onChange={(e) => setInput(e.target.value)}/>
+            <button onClick={addTask}>Add Task</button>
+            <h1>Your Task</h1>
+            <ul>
+                {tasks.map(task => (
+                    <li>{task}</li>
+                ))}
+            </ul>
         </>
     )
 }
