76cfe567cf3f06b1f48214431cef6f83bffddcd1
hagenimanasamuel <smlhagenimana@gmail.com>
2026-09-23 07:11:38 +0200

    added the button and functionalities to change the list type

--- learning-react/src/components/Learning-exercises/ToDo.jsx
+++ learning-react/src/components/Learning-exercises/ToDo.jsx
@@ -4,15 +4,14 @@ import "./todo.css";
 export default function ToDo() {
     const [input, setInput] = useState('');
     const [tasks, setTasks] = useState([]);
+    const [listType, setListType] = useState('ul');
 
     function addTask() {
         setTasks([...tasks, input]);
         setInput('');
     }
 
-    useEffect(() => {
-
-    })
+    const Listing = listType;
 
     return (
         <>
@@ -20,11 +19,12 @@ export default function ToDo() {
             <input type="text" placeholder="input task" value={input} onChange={(e) => setInput(e.target.value)}/>
             <button onClick={addTask}>Add Task</button>
             <h1>Your Task</h1>
-            <ul>
+            <Listing>
                 {tasks.map(task => (
                     <li>{task}</li>
                 ))}
-            </ul>
+            </Listing>
+            <button onClick={() => setListType(listType === "ul" ? "ol" : "ul")}>Change List type</button>
         </>
     )
 }
