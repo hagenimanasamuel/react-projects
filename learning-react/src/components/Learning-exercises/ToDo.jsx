2702095108d8481dc6bcd216ebc2d1fdacc04473
hagenimanasamuel <smlhagenimana@gmail.com>
2026-09-24 19:49:25 +0200

    added the functionalities for those message notifications to works and also added the needed states and needed possible functions

--- learning-react/src/components/Learning-exercises/ToDo.jsx
+++ learning-react/src/components/Learning-exercises/ToDo.jsx
@@ -1,33 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./todo.css";
 
 export default function ToDo() {
     const [input, setInput] = useState('');
     const [tasks, setTasks] = useState([]);
     const [listType, setListType] = useState('ul');
+    const [message, setMessage] = useState('');
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        if(!visible) return;
+
+        const messageTimer = setTimeout(() => {
+            setVisible(false);
+        }, 2000);
+    },[visible, message]);
 
     function addTask() {
         setTasks([...tasks, {text: input, done: false}]);
         setInput('');
+        setMessage("Task added Successfully");
+        setVisible(true);
     }
 
     function toggleDone(index) {
         setTasks(tasks.map((task, i) => {
             return i === index ? {...task, done: !task.done} : task
         }))
+        setMessage("Task marked as done");
+        setVisible(true);
     }
 
     function removeItem(index) {
         setTasks(tasks.filter((task, i) => {
             return i !== index;
         }))
+        setMessage('Task removed successfully');
+        setVisible(true);
     } 
 
     const Listing = listType;
 
     return (
         <>
-            <span className="message">Done!!</span>
+            {visible && <span className="message">{message}</span>}
             <h1>To-Do App</h1>
             <input type="text" placeholder="input task" value={input} onChange={(e) => setInput(e.target.value)}/>
             <button onClick={addTask}>Add Task</button>
